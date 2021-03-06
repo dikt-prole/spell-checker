9c848b9a2e5d19dd0da357717e87b0f349c819a5
dikt-prole <dikt-prole@mail.ru>
2021-03-07 02:41:56 +0300

    submit code

---
+++ SpellChecker/SpellCheckerForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace SpellChecker
+{
+    partial class SpellCheckerForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.text_tb = new System.Windows.Forms.RichTextBox();
+            this.check_btn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.text_tb, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.check_btn, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // text_tb
+            // 
+            this.text_tb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.text_tb.Location = new System.Drawing.Point(3, 3);
+            this.text_tb.Name = "text_tb";
+            this.text_tb.Size = new System.Drawing.Size(794, 414);
+            this.text_tb.TabIndex = 0;
+            this.text_tb.Text = "";
+            // 
+            // check_btn
+            // 
+            this.check_btn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.check_btn.Location = new System.Drawing.Point(697, 423);
+            this.check_btn.Name = "check_btn";
+            this.check_btn.Size = new System.Drawing.Size(100, 24);
+            this.check_btn.TabIndex = 1;
+            this.check_btn.Text = "Проверить";
+            this.check_btn.UseVisualStyleBackColor = true;
+            this.check_btn.Click += new System.EventHandler(this.check_btn_Click);
+            // 
+            // SpellCheckerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "SpellCheckerForm";
+            this.Text = "Проверка правописания";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox text_tb;
+        private System.Windows.Forms.Button check_btn;
+    }
+}
+
---
+++ SpellChecker/SpellCheckerForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YandexLinguistics.NET.Speller;
+
+namespace SpellChecker
+{
+    public partial class SpellCheckerForm : Form
+    {
+        private readonly SpellerService _speller = new SpellerService();
+
+        public SpellCheckerForm()
+        {
+            InitializeComponent();
+
+            using (var stream = File.OpenRead("spell-check.ico"))
+            {
+                Icon = new Icon(stream);
+            }
+        }
+
+        private async void check_btn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(text_tb.Text)) return;
+
+            var wordsQueue = new Queue<string>(text_tb.Text.Split(' ', StringSplitOptions.RemoveEmptyEntries));
+
+            var index = 0;
+            while (wordsQueue.Any())
+            {
+                var sb = new StringBuilder();
+                while (sb.Length < 500 && wordsQueue.Any())
+                {
+                    sb.Append(wordsQueue.Dequeue());
+                    sb.Append(' ');
+                }
+
+                var token = sb.ToString();
+                var errors = await _speller.CheckTextAsync(token);
+                foreach (var error in errors)
+                {
+                    text_tb.SelectionStart = index + error.Position;
+                    text_tb.SelectionLength = error.Length;
+                    text_tb.SelectionBackColor = Color.Yellow;
+                }
+
+                index += token.Length;
+            }
+        }
+    }
+}
